9afc0d4c9bd7c9ed6ed30847b4d86456d610dbf4
alisson <alissonricarte7575@gmail.com>
2024-09-20 16:55:56 -0300

    inicioando o projeto

---
+++ cliente.c
@@ -0,0 +1,10 @@
+#include "cliente.h"
+
+struct reserva{
+    char nome_do_solitante[50];
+    int data_de_reserva;
+    int horario_de_inicio;
+    int horario_de_termino;
+    char destino[50];
+};
+
---
+++ cliente.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//Estrututa para fazer a reserva do cliente
+typedef struct reserva Reserva;
