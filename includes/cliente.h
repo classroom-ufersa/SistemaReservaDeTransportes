fbd9c6218251970a867f467f105aa3dbadeafa04
alisson <alissonricarte7575@gmail.com>
2024-09-21 17:39:39 -0300

    função de nó

--- includes/cliente.h
+++ includes/cliente.h
@@ -4,3 +4,12 @@
 
 //Estrututa para fazer a reserva do cliente
 typedef struct reserva Reserva;
+
+//Estrutura para fazar lista
+typedef struct no No;
+
+//Função para criar o primeiro Nó
+No * CriarNo(char * NomeSolitante, int Data, int HoraInicio , int HoraTermino , char * Destino);
+
+//Função para ligar um Nó no outro
+No * AdicionarCliente(No * lista,char * NomeSolitante, int Data, int HoraInicio , int HoraTermino , char * Destino);
